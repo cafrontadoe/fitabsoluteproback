8d95a1192d7ffc2204f2c961838a6a8fd780df0e
Camilo Andrés Frontado Escobar <redesjavacamilo@gmail.com>
2023-12-26 18:53:23 +0000

    delete orderplace suscriber

--- src/subscribers/order-placed.ts
+++ src/subscribers/order-placed.ts
@@ -1,39 +1,39 @@
-import { 
-    type SubscriberConfig, 
-    type SubscriberArgs,
-    OrderService,
-  } from "@medusajs/medusa"
+// import { 
+//     type SubscriberConfig, 
+//     type SubscriberArgs,
+//     OrderService,
+//   } from "@medusajs/medusa"
   
-  export default async function handleOrderPlaced({ 
-    data, eventName, container, pluginOptions, 
-  }: SubscriberArgs<Record<string, string>>) {
-    const sendGridService = container.resolve("sendgridService")
-    const orderService: OrderService = container.resolve(
-      "orderService"
-    )
+//   export default async function handleOrderPlaced({ 
+//     data, eventName, container, pluginOptions, 
+//   }: SubscriberArgs<Record<string, string>>) {
+//     const sendGridService = container.resolve("sendgridService")
+//     const orderService: OrderService = container.resolve(
+//       "orderService"
+//     )
   
-    const order = await orderService.retrieve(data.id, {
-      // you can include other relations as well
-      relations: ["items"],
-    })
+//     const order = await orderService.retrieve(data.id, {
+//       // you can include other relations as well
+//       relations: ["items"],
+//     })
   
-    console.log('================ZZZZZZZZZZZZZZZZZzzzzzzzzzzZZZZZZZZZZZZZZ');
-    console.log('order???????????????????????', order);
-    sendGridService.sendEmail({
-      templateId: "order-confirmation",
-      from: "fitabsolutepro@gmal.com",
-      to: order.email,
-      dynamic_template_data: {
-        // any data necessary for your template...
-        items: order.items,
-        status: order.status,
-      },
-    })
-  }
+//     console.log('================ZZZZZZZZZZZZZZZZZzzzzzzzzzzZZZZZZZZZZZZZZ');
+//     console.log('order???????????????????????', order);
+//     sendGridService.sendEmail({
+//       templateId: "order-confirmation",
+//       from: "fitabsolutepro@gmal.com",
+//       to: order.email,
+//       dynamic_template_data: {
+//         // any data necessary for your template...
+//         items: order.items,
+//         status: order.status,
+//       },
+//     })
+//   }
   
-  export const config: SubscriberConfig = {
-    event: OrderService.Events.PLACED,
-    context: {
-      subscriberId: "order-placed-handler",
-    },
-  }
+//   export const config: SubscriberConfig = {
+//     event: OrderService.Events.PLACED,
+//     context: {
+//       subscriberId: "order-placed-handler",
+//     },
+//   }
