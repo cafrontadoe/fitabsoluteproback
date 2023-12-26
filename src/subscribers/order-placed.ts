410654efab642894f2bf3d7c141abf08e1263795
Camilo Andrés Frontado Escobar <redesjavacamilo@gmail.com>
2023-12-26 16:35:45 +0000

    Revert "Revert "Revert "deleted gridsend loader and console suscriber"""
    This reverts commit 77132ee36e206afa8828705541ff2efb0f194056.

--- src/subscribers/order-placed.ts
+++ src/subscribers/order-placed.ts
@@ -17,8 +17,6 @@ import {
       relations: ["items"],
     })
   
-    console.log('================ZZZZZZZZZZZZZZZZZzzzzzzzzzzZZZZZZZZZZZZZZ');
-    console.log('order???????????????????????', order);
     sendGridService.sendEmail({
       templateId: "order-confirmation",
       from: "fitabsolutepro@gmal.com",
