bc88e1e8e6ca0bd26459ce0554bfeb981fc930d5
Camilo Andrés Frontado Escobar <redesjavacamilo@gmail.com>
2023-12-28 18:21:23 +0000

    fix 409 error

---
+++ src/subscribers/order-placed.ts
@@ -0,0 +1,33 @@
+import { 
+    type SubscriberConfig, 
+    type SubscriberArgs,
+    OrderService,
+  } from "@medusajs/medusa"
+  
+  export default async function handleOrderPlaced({ 
+    data, eventName, container, pluginOptions, 
+  }: SubscriberArgs<Record<string, string>>) {
+    const sendGridService = container.resolve("sendgridService")
+    const orderService: OrderService = container.resolve(
+      "orderService"
+    )
+  
+    const order = await orderService.retrieve(data.id, {
+      // you can include other relations as well
+      relations: ["items"],
+    })
+
+     // auto-capture payment
+     if (order.payment_status !== "captured") {
+        await orderService.capturePayment(order.id);
+    }
+  
+    
+  }
+  
+  export const config: SubscriberConfig = {
+    event: OrderService.Events.PLACED,
+    context: {
+      subscriberId: "order-capture-fulfill",
+    },
+  }
