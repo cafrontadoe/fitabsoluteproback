8f9b1052e5029df7a2b66165ac76a5cb3b1d1e5c
Camilo Andrés Frontado Escobar <redesjavacamilo@gmail.com>
2024-01-03 17:16:25 +0000

    fix payment 409

--- src/subscribers/order-placed.ts
+++ src/subscribers/order-placed.ts
@@ -20,17 +20,47 @@ class OrderPlacedSubscriber {
     }
 
     handleOrderPlaced = async (data) => {
-        const order = await this.orderService.retrieve(data.id, { relations: ["items"] })
-
-        // auto-capture payment
-        console.log('-------------------------------------------------------------------zzzzzzzzzzzz');
-        console.log('order.payment_status:'+order.payment_status);
-        if (order.payment_status !== "captured") {
-            console.log('-------------------------------------------------------------------entra sok');
-
-            await this.orderService.capturePayment(order.id)
-        }
+      let retries = 2;  // Number of retries you want
+  
+      while (retries > 0) {
+          try {
+              const order = await this.orderService.retrieve(data.id, { relations: ["items"] });
+  
+              // auto-capture payment
+              console.log('-------------------------------------------------------------------zzzzzzzzzzzz');
+              console.log('order.payment_status:'+order.payment_status);
+              
+              if (order.payment_status !== "captured") {
+                  console.log('-------------------------------------------------------------------entra sok');
+                 
+                  const result = await this.orderService.capturePayment(order.id);
+  
+                  console.log('resullt: ' + result);
+  
+                  // Break the loop if successful
+                  break;
+              }
+          } catch (error) {
+              if (error.response && error.response.status === 409) {
+                  // Handle 409 error (Conflict) - You may log it or do additional actions
+                  console.error('Conflict error (409) - Retrying...');
+                  
+                  // Decrement the number of retries
+                  retries--;
+  
+                  // You may add a delay between retries if needed
+                  await new Promise(resolve => setTimeout(resolve, 1000));  // 1 second delay
+              } else {
+                  // Handle other errors
+                  console.error('Unexpected error:', error);
+                  break;  // Exit the loop on unexpected errors
+              }
+          }
+      }
   };
+
+
+
 }
 
 export default OrderPlacedSubscriber;
