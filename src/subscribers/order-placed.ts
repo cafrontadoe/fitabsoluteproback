655c0ed4e37b3290c5a7168faf56ae853e1141e4
Camilo Andrés Frontado Escobar <redesjavacamilo@gmail.com>
2024-01-04 18:33:15 +0000

    add try catch

--- src/subscribers/order-placed.ts
+++ src/subscribers/order-placed.ts
@@ -1,4 +1,9 @@
-import { ConfigModule, EventBusService, CartService, OrderService } from "@medusajs/medusa";
+import {
+  ConfigModule,
+  EventBusService,
+  CartService,
+  OrderService,
+} from "@medusajs/medusa";
 
 type InjectedDependencies = {
   eventBusService: EventBusService;
@@ -8,38 +13,43 @@ type InjectedDependencies = {
 };
 
 class OrderPlacedSubscriber {
-    cartService: any;
-    orderService: any;
-    orderRepository: any;
-    constructor({ eventBusService, cartService, orderService, orderRepository }) {
-        this.cartService = cartService
-        this.orderService = orderService
-        this.orderRepository = orderRepository
+  cartService: any;
+  orderService: any;
+  orderRepository: any;
+  constructor({ eventBusService, cartService, orderService, orderRepository }) {
+    this.cartService = cartService;
+    this.orderService = orderService;
+    this.orderRepository = orderRepository;
 
-        eventBusService.subscribe("order.placed", this.handleOrderPlaced, { subscriberId: "order-capture-fulfill" })
-    }
+    eventBusService.subscribe("order.placed", this.handleOrderPlaced, {
+      subscriberId: "order-capture-fulfill",
+    });
+  }
 
-    handleOrderPlaced = async (data) => {
-        const order = await this.orderService.retrieve(data.id, { relations: ["items"] })
+  handleOrderPlaced = async (data) => {
+    const order = await this.orderService.retrieve(data.id, {
+      relations: ["items"],
+    });
 
-        // auto-capture payment
-        console.log('-------------------------------------------------------------------zzzzzzzzzzzz');
-        console.log('order.payment_status:'+order.payment_status);
-        if (order.payment_status !== "captured") {
-
-           console.log('-------------------------------------------------------------------entra sok');
-           try{
-             await this.orderService.capturePayment(order.id);
-             
-            } catch (error: any) {
-              console.log('error: '+error);
-              if (error.response && error.response.status === 409) {
-                await new Promise(resolve => setTimeout(resolve, 200));  // 1 second delay
-                await this.orderService.capturePayment(order.id);
-
-            }
-
+    // auto-capture payment
+    console.log(
+      "-------------------------------------------------------------------zzzzzzzzzzzz"
+    );
+    console.log("order.payment_status:" + order.payment_status);
+    if (order.payment_status !== "captured") {
+      console.log(
+        "-------------------------------------------------------------------entra sok"
+      );
+      try {
+        await this.orderService.capturePayment(order.id);
+      } catch (error: any) {
+        console.log("error: " + error);
+        if (error.response && error.response.status === 409) {
+          await new Promise((resolve) => setTimeout(resolve, 200)); // 1 second delay
+          await this.orderService.capturePayment(order.id);
         }
+      }
+    }
   };
 }
 
